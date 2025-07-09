97ad6b96078ebc03e7dae18b436c36906919ba6e
DanieL <kopylenkod@gmail.com>
2025-07-09 15:46:15 -0400

    condensed the interface/implementation pair into it's own class

--- service/GradeCalculator.java
+++ service/GradeCalculator.java
@@ -1,5 +1,12 @@
 package implementation.service;
 
-public interface GradeCalculator {
-  double calculateFinalGrade(double[] tests, double finalExam);
+import implementation.util.Constants;
+
+public class GradeCalculator {
+  double calculateFinalGrade(double[] tests, double finalExam) {
+    // TODO: do some validation on the inputs
+
+    return (tests[0] + tests[1] + tests[2]) * Constants.TEST_WEIGHT +
+           finalExam * Constants.FINAL_EXAM_WEIGHT;
+  }
 }
